4286c1101ee9e489975f67179b151b420ede03ef
nicolerrr1 <21-08560@g.batstate-u.edu.ph>
2026-07-06 08:40:46 +0800

    js updated

--- expenseiq/resources/js/app.js
+++ expenseiq/resources/js/app.js
@@ -6,4 +6,17 @@ import Chart from 'chart.js/auto';
 window.Chart = Chart;
 
 console.log("APP JS LOADED");
+console.log(typeof window.openModal);
 console.log(window.Chart);
+
+window.openModal = function(id) {
+
+    document.getElementById(id).classList.remove('hidden');
+
+}
+
+window.closeModal = function(id) {
+
+    document.getElementById(id).classList.add('hidden');
+
+}
